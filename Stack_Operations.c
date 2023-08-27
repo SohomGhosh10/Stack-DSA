71311185f0f7eca7e13de61b30d13412d25f7521
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-08-27 21:22:01 +0530

    Update Stack_Operations.c

--- Stack_Operations.c
+++ Stack_Operations.c
@@ -7,8 +7,8 @@ struct stack{
     int* arr;
 };
 
-int isEmpty(struct stack * ptr){
-    if(ptr->top == -1){
+int isEmpty(struct stack * ptr){ // struct stack type pointer
+    if(ptr->top == -1){ // checking for empty
         return 1;
     }
     else{
@@ -17,7 +17,7 @@ int isEmpty(struct stack * ptr){
 }
 
 int isFull(struct stack * ptr){
-    if(ptr->top == ptr->size - 1){
+    if(ptr->top == ptr->size - 1){ // checking for full
         return 1;
     }
     else{
@@ -26,11 +26,13 @@ int isFull(struct stack * ptr){
 }
 
 int main(){
-    struct stack* s = (struct stack*)malloc(sizeof(struct stack));
+    struct stack* s = (struct stack*)malloc(sizeof(struct stack)); // struct stack * s stores the address of the whole structure
     s->size = 6;
     s->top = -1;
     s->arr = (int *)malloc(s->size * sizeof(int));
     printf("Stack created successfully\n");
-    printf("%d\n",isEmpty(s));
-    printf("%d\n",isFull(s));
+    printf("%d\n",isEmpty(s)); // 1
+    printf("%d\n",isFull(s)); // 0
+
+    return 0;
 }
