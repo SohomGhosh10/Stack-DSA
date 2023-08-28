2d3d1e8d8ec21690336d9a5c377a61ddcfc08a23
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-08-28 23:54:14 +0530

    Update Stack_Overflow.c

--- Stack_Overflow.c
+++ Stack_Overflow.c
@@ -26,37 +26,37 @@ int isEmpty(struct stack * ptr){
 }
 
 void push(struct stack * ptr , int val){
-    if (isFull(ptr)){
+    if (isFull(ptr)){ // condition for overflow
         printf("Stack overflow\n");
         printf("Cannot insert %d to the stack" ,val);
     }
     else{
-        ptr->top++;
-        ptr->arr[ptr->top] = val;
+        ptr->top++; // incrementation of top index 
+        ptr->arr[ptr->top] = val; 
     }
 }
 
 int main(){
-    struct stack* s = (struct stack*) malloc(sizeof(struct stack));
-    s->size = 10;
-    s->top = -1;
-    s->arr = (int*) malloc(s->size * sizeof(int*));
+    struct stack* s = (struct stack*) malloc(sizeof(struct stack)); // stack pointer
+    s->size = 10; // size declaration
+    s->top = -1; // top index
+    s->arr = (int*) malloc(s->size * sizeof(int*)); // stack creation
 
     printf("Stack created successfully\n");
     printf("Before pushing: %d\n",isEmpty(s));
     printf("Before Pushing: %d\n",isFull(s));
 
-    push(s , 56);
-    push(s , 56);
-    push(s , 56);
-    push(s , 56);
-    push(s , 56);
-    push(s , 56);
-    push(s , 56);
-    push(s , 56);
-    push(s , 56);
-    push(s , 56); // Stack is full
-    push(s , 57); // Stack overflow as size was 10
+    push(s , 6);
+    push(s , 13);
+    push(s , 98);
+    push(s , 45);
+    push(s , 49);
+    push(s , 57);
+    push(s , 78);
+    push(s , 87);
+    push(s , 52);
+    push(s , 23); // Stack is full
+    push(s , 44); // Stack overflow as size was 10
 
     printf("After pushing empty: %d\n",isEmpty(s));
     printf("After pushing full:%d\n",isFull(s));
